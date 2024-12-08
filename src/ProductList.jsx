ea552638238ed749497990d90c840ab404a07197
craigcratergames <craig_jeffcoat@yahoo.com>
2024-12-08 09:25:14 -0500

    Finished Task

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,7 @@ function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();  // Initialize dispatch function
     const cart = useSelector(state => state.cart.items);
 
 
